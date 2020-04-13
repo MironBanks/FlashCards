be858beec62ac149f275ca5d2c05024a25d351cd
Miron Banks <madapegarage@gmail.com>
2020-04-13 23:22:47 +0200

    sample data

--- src/App.js
+++ src/App.js
@@ -6,4 +6,40 @@ function App() {
   );
 }
 
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    questions: 'What is 2 + 2?',
+    answer: 4,
+    options: [
+      '1',
+      '5',
+      '4',
+      '6'
+    ]
+  },
+  {
+    id: 2,
+    questions: 'Question number 2',
+    answer: 5,
+    options: [
+      '1',
+      '5',
+      '4',
+      '6'
+    ]
+  },
+  {
+    id: 3,
+    questions: 'Question number 3',
+    answer: 'test',
+    options: [
+      '1',
+      '5',
+      '4',
+      '6'
+    ]
+  }
+]
+
 export default App;
